cf9d0118af2495b6558b291373e9f2ac27138b97
mcverter <mitchell.verter@gmail.com>
2020-04-08 13:36:57 -0400

    using keys

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var jsonBlob = require('../database/index.json');
+var jsonBlob = require('../database/data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
