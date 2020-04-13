461ee444cfd953fd75c5d1d20153b0133562e44f
mcverter <mitchell.verter@gmail.com>
2020-04-13 19:20:56 -0400

    routes for shareable

--- routes/shareable.js
+++ routes/shareable.js
@@ -4,16 +4,14 @@ var regions = require('../database/data.json').regions;
 
 /* GET home page. */
 router.get('/:id', function(req, res, next) {
-    const bid = req.query.id;
     const id = parseInt(req.params.id)
-        console.log('hello get shareable')
     for (region in regions){
         let shareable = regions[region].shareables.find(s=>s.id === id)
         if (shareable) {
-            res.json(shareable);
+            return res.json(shareable);
         }
     }
-    res.json({"message": `Shareable Not Found: ${id}`})
+    return res.json({"message": `Shareable Not Found: ${id}`})
 });
 
 module.exports = router;
